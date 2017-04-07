4c719be8a4e2cff22fae9fc8b550a9d6d9ea29f6
piaopiao1201 <576883737@qq.com>
2017-04-07 16:44:11 +0800

    second commit

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,5 @@
 require('./hichat/hichat.js');
-// require('../css/index.css');
+
 require('../css/index.styl');
 require('../css/index2.styl');
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
